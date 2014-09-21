ae36bc07b4be92e74c8ece10650ead38ec4e7908
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-09-21 04:08:53 +0100

    Added inverse for interface on prefs model

--- admin/app/models/pref.js
+++ admin/app/models/pref.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   type: DS.attr('string'),
   value: DS.attr('string'),
-  interface: DS.belongsTo('interface'),
+  interface: DS.belongsTo('interface', {inverse: 'prefs'}),
   plugin: DS.belongsTo('plugin', {inverse: 'prefs'}),
   group: DS.attr('string'),
   isPassword: function(){
