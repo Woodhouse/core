97bad95a5c21cd2b2538c950855528e3328b66ab
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-10-04 23:16:08 +0100

    Changed say plugin to be prototypal

--- plugins/say/index.js
+++ plugins/say/index.js
@@ -1,20 +1,20 @@
-module.exports = function(){
-
+var say = function(){
     this.name = 'say';
     this.displayname = 'Say';
     this.description = 'Make woodhouse say things back to you';
-
-    this.init = function(){
-        var self = this;
-        this.listen('(say|tell) (.+?)', 'standard', function(from, interface, params){
-            self.sendMessage(params[1], interface, from);
-        })
-
-        this.listen('who steals cars\\?', 'standard', function(from, interface){
-            self.sendMessage('GYPPOS!', interface, from);
-        })
-
-    }
-
-    return this;
 }
+
+
+say.prototype.init = function(){
+    var self = this;
+    this.listen('(say|tell) (.+?)', 'standard', function(from, interface, params){
+        self.sendMessage(params[1], interface, from);
+    })
+
+    this.listen('who steals cars\\?', 'standard', function(from, interface){
+        self.sendMessage('GYPPOS!', interface, from);
+    })
+
+}
+
+module.exports = say;
