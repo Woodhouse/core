9c6082f6eeb8585d885a7bf98f780fb8571ddab7
Luke Bonaccorsi <luke@lukeb.co.uk>
2016-03-19 14:17:13 +0000

    Removed admin panel in preparation for rewrite as part of V2

--- admin/app/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-    model: function() {
-        return this.store.filter('pref', null, function(pref){
-            return pref.get('interface') === null && pref.get('plugin') === null;
-        });
-    }
-});
