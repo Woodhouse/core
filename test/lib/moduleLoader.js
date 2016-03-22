c4390e8bbb2d428614f3b54784cd3f34e40b4a3b
Luke Bonaccorsi <luke@lukeb.co.uk>
2016-03-22 00:57:09 +0000

    Added loading of modules, extending with the core methods and tests

--- test/lib/moduleLoader.js
+++ test/lib/moduleLoader.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const chaiAsPromised = require("chai-as-promised");
 const mockery = require('mockery');
+const bluebird = require('bluebird');
 
 chai.use(chaiAsPromised);
 
@@ -10,6 +11,59 @@ const expect = chai.expect;
 
 describe('Module Loader', function() {
     let moduleLoader;
+
+    const mockDispatcher = {
+        addMessageSender: () => {
+            return 'addMessageSender';
+        },
+        messageRecieved: () => {
+            return 'messageRecieved';
+        },
+        listen: () => {
+            return 'listen';
+        },
+        sendMessage: () => {
+            return 'sendMessage';
+        },
+        loadListenerAliases: () => {
+            return 'loadListenerAliases';
+        }
+    };
+
+    const mockModulePrefs = {
+        get: () => {
+            return 'get';
+        },
+        isEnabled: (type, name) => {
+            if (name === 'testModule1') {
+                return bluebird.resolve(true);
+            } else if (name === 'testModule2') {
+                return bluebird.resolve(false)
+            } else if (name === 'testModule3') {
+                let error = new Error('No preferences found for ' + name);
+                error.shortCode = 'no-prefs';
+                return bluebird.reject(error);
+            } else {
+                return bluebird.reject(new Error('error!'));
+            }
+        },
+        addModule: () => {
+            return bluebird.resolve();
+        }
+    };
+
+    const mockCron = {
+        add: () => {
+            return 'add';
+        },
+        remove: () => {
+            return 'remove';
+        },
+        registerHandler: () => {
+            return 'registerHandler';
+        }
+    };
+
     before(function() {
         mockery.enable({
             warnOnReplace: false,
@@ -18,30 +72,106 @@ describe('Module Loader', function() {
         });
 
         mockery.registerMock('fs', {
-            readdir() {
-                return [
+            readdir: (path, callback) => {
+                callback(null, [
                     'folder1',
                     'folder2',
                     'file1'
-                ]
+                ]);
             },
-            stat(file) {
-                return {
-                    isDirectory(){
-                        return file === 'file1';
+            stat: (path, callback) => {
+                callback(null, {
+                    isDirectory: () => {
+                        return path !== 'test/file1';
                     }
-                }
+                });
+            }
+        });
+
+        mockery.registerMock('../plugin/testModule1', class testModule1 {
+            constructor() {
+                this.name = 'testModule1';
+            }
+
+            init() {
+                this.initCalled = true;
+            }
+        });
+
+        mockery.registerMock('../plugin/testModule2', class testModule2 {
+            constructor() {
+                this.name = 'testModule2';
+            }
+        });
+
+        mockery.registerMock('../plugin/testModule3', class testModule3 {
+            constructor() {
+                this.name = 'testModule3';
+            }
+        });
+
+        mockery.registerMock('../plugin/testModule4', class testModule4 {
+            constructor() {
+                this.name = 'testModule4';
             }
         });
 
         const moduleLoaderClass = require("../../lib/moduleLoader.js");
-        moduleLoader = new moduleLoaderClass({}, {});
+        moduleLoader = new moduleLoaderClass(mockDispatcher, mockModulePrefs, mockCron);
     });
 
     it('getmodulelist should only return folders', function() {
         const moduleList = moduleLoader.getModuleList('test');
 
-        expect(moduleList).to.eventually.have.lengthOf(2);
-        expect(moduleList).to.eventually.not.contain('file1');
-    })
-});
+        return expect(moduleList).to.eventually.deep.equal([
+            {type: 'test', module: 'folder1'},
+            {type: 'test', module: 'folder2'}
+        ]);
+    });
+
+    it('addCoreMethods should make methods available on module', function() {
+        const module = {}
+        moduleLoader.addCoreMethods(module, 'test');
+
+        expect(module).to.have.property('addMessageSender');
+        expect(module.addMessageSender()).to.equal('addMessageSender');
+        expect(module).to.have.property('messageRecieved');
+        expect(module.messageRecieved()).to.equal('messageRecieved');
+        expect(module).to.have.property('listen');
+        expect(module.listen()).to.equal('listen');
+        expect(module).to.have.property('sendMessage');
+        expect(module.sendMessage()).to.equal('sendMessage');
+        expect(module).to.have.property('registerCronHandler');
+        expect(module.registerCronHandler()).to.equal('registerHandler');
+        expect(module).to.have.property('addCronJob');
+        expect(module.addCronJob()).to.equal('add');
+        expect(module).to.have.property('removeCronJob');
+        expect(module.removeCronJob()).to.equal('remove');
+        expect(module).to.have.property('getPref');
+        expect(module.getPref()).to.equal('get');
+    });
+
+    it('loadModule should load the module if it is enabled', function() {
+        const module = moduleLoader.loadModule('testModule1', 'plugin');
+
+        return expect(module).to.eventually.have.property('initCalled', true)
+    });
+
+    it('loadModule should not load the module if it is disabled', function() {
+        const module = moduleLoader.loadModule('testModule2', 'plugin');
+
+        return expect(module).to.eventually.be.false;
+    });
+
+    it('loadModule should not load the module if it does not exist in the db', function() {
+        const module = moduleLoader.loadModule('testModule3', 'plugin');
+
+        return expect(module).to.eventually.be.false;
+    });
+
+    it('loadModule should rethrow any exceptions besides the no-prefs one', function() {
+        const module = moduleLoader.loadModule('testModule4', 'plugin');
+
+        return expect(module).to.be.rejectedWith(Error, 'error!');
+    });
+})
