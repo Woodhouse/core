60bbbce181e1be31eb15637dfa3c5fd54d1f43d8
Luke Bonaccorsi <luke.bonaccorsi@skybettingandgaming.com>
2016-10-21 15:26:13 +0100

    Removed the constructor from all of the upgrade scripts because it was always the same, added the properties within the upgrade.js instead

--- upgrade_scripts/0.1.1.js
+++ upgrade_scripts/0.1.1.js
@@ -1,13 +1,4 @@
 class upgrade {
-	constructor(systemData, interfacePrefData, pluginPrefData, basePrefData, usersData, cronData) {
-		this.systemData = systemData;
-        this.interfacePrefData = interfacePrefData;
-        this.pluginPrefData = pluginPrefData;
-        this.basePrefData = basePrefData;
-        this.usersData = usersData;
-        this.cronData = cronData;
-	}
-
 	run() {
 		return this.interfacePrefData.updateAsync({}, {$set: {default_permission: ''}}, { multi: true });
 	}
--- upgrade_scripts/1.0.1.js
+++ upgrade_scripts/1.0.1.js
@@ -1,13 +1,4 @@
 class upgrade {
-	constructor(systemData, interfacePrefData, pluginPrefData, basePrefData, usersData, cronData) {
-		this.systemData = systemData;
-        this.interfacePrefData = interfacePrefData;
-        this.pluginPrefData = pluginPrefData;
-        this.basePrefData = basePrefData;
-        this.usersData = usersData;
-        this.cronData = cronData;
-	}
-
 	run() {
 		return this.usersData.findAsync({}).map((user) => {
 			return this.usersData.insertAsync({
